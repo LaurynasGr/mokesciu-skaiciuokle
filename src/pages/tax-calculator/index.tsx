5695431bfaf05efe3dd581fafc1dc4b3018cdbe1
Laurynas Grigutis <laurynasgr@gmail.com>
2026-01-05 17:22:50 +0200

    Moved stuff to `TaxCalculatorPage`

---
+++ src/pages/tax-calculator/index.tsx
@@ -0,0 +1,12 @@
+import { Calculator } from 'lucide-react';
+
+export function TaxCalculatorPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-black flex items-center justify-center gap-2">
+        <Calculator className="size-8" />
+        Sveiki atvykę į Mokesčių Skaičiuoklę
+      </h1>
+    </div>
+  );
+}
